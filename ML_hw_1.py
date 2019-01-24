5b45930883fa3933e3b55667e57c006c50ff927f
PortWhisperer <davidskyniu@gmail.com>
2019-01-24 04:16:20 -0600

    functions tested working, now can't get plots to show up on each run of graph function EXCEPT for all 4 of the final plt.show() command.

--- ML_hw_1.py
+++ ML_hw_1.py
@@ -1,29 +1,25 @@
 import numpy as np
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 from sklearn import neighbors
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 
 
 def eval_Knn_Performance(var_list):
-    if len(var_list[0]) == 5:
-        y_i, y_i_test, true_fx_yvals_train, x_i_p, x_i_p_test = var_list[0]
+    if len(var_list) == 6:
+        y_i, y_i_test, true_fx_yvals_train, op_name, xi, xi_test = var_list
     else:
-        y_i, y_i_test, true_fx_yvals_train = var_list[0]
-
-    if x_i_p and x_i_p_test:
-        xi_test, xi = var_list[3], var_list[4]  # pull in x values from list if avail
-    else:
-        xi_test, xi = x_i_test, x_i  # else pull in x values from global scope and avoid shadowing
+        y_i, y_i_test, true_fx_yvals_train, op_name = var_list
+        xi, xi_test = x_i, x_i_test  # else pull in x values from global scope and avoid shadowing
 
     # create points for the true  f(x) function
     true_fx_x_vals = np.linspace(-1, 1, 10000)
     true_fx_y_vals = true_fx_yvals_train
 
-    # matplotlib.pyplot.scatter(x_i_test, y_i_test)
-    # matplotlib.pyplot.scatter(x_i, y_i)
-    # matplotlib.pyplot.plot(true_fx_xvals, true_fx_yvals, 'r--', label='f')  # Plot true f(x) function
-    # matplotlib.pyplot.show()
+    # plt.scatter(x_i_test, y_i_test)
+    # plt.scatter(x_i, y_i)
+    # plt.plot(true_fx_xvals, true_fx_yvals, 'r--', label='f')  # Plot true f(x) function
+    # plt.show()
 
     true_fx_xvals_trainset = np.linspace(-1, 1, 100)
 
@@ -36,18 +32,18 @@ def eval_Knn_Performance(var_list):
     # https://stackoverflow.com/questions/22239691/code-for-line-of-best-fit-of-a-scatter-plot-in-python
     # #########################################################################
 
-    matplotlib.pyplot.plot(np.unique(xi), np.poly1d(np.polyfit(xi, y_i, 1))(np.unique(xi)), 'orange')
+    # plt.plot(np.unique(xi), np.poly1d(np.polyfit(xi, y_i, 1))(np.unique(xi)), 'orange')
 
-    # Plot k-NN for k=2 through k=15
+    # Get RMSE values for different K
     # code found at https://www.analyticsvidhya.com/blog/2018/08/k-nearest-neighbor-introduction-regression-python/
 
     rmse_val = []  # to store rmse values for different k
     rmse_val_train = []
 
     for K in range(1, 15):
-        K = K + 1
+        k = K + 1
 
-        model = neighbors.KNeighborsRegressor(n_neighbors=K)  # create the model
+        model = neighbors.KNeighborsRegressor(n_neighbors=k)  # create the model
         model.fit(xi.reshape(-1, 1), y_i)  # fit the KNN model
 
         pred = model.predict(xi_test.reshape(-1, 1))
@@ -73,14 +69,15 @@ def eval_Knn_Performance(var_list):
     # plot results
     # RMSE vs log(k)
     k = np.linspace(2.0, 15.0, 14)  # create for matplotlib plotting purposes
-    fig, (ax1, ax2) = matplotlib.pyplot.subplots(2, 1)  # create subplots
+    fig, (ax1, ax2) = plt.subplots(2, 1)  # create subplots
     fig.subplots_adjust(hspace=0.5)
-
+    fig.suptitle(op_name + 'RMSE analysis')
     ax1.plot(np.log(1.0 / k), np.array(rmse_val), c='r', label='RMSE')
     ax1.plot(np.log(1.0 / k), np.array(rmse_val_train), c='black', label='In sample RMSE')
     ax1.axhline(y=error_lin_reg_test, c='g', dashes=(5, 1), label="Lin Reg RMSE")
     ax1.set_xlabel('Log(1/k)')
     ax1.set_ylabel('RMSE')
+    ax1.set_title('Performance versus log(1/k)')
     ax1.legend(loc='upper left')
 
     # RMSE vs K
@@ -89,35 +86,34 @@ def eval_Knn_Performance(var_list):
     ax2.axhline(y=error_lin_reg_test, c='g', dashes=(5, 1), label="Lin Reg RMSE")
     ax2.set_xlabel('K')
     ax2.set_ylabel('RMSE')
+    ax2.set_title('Performance versus K values')
     ax2.legend(loc='upper left')
-    matplotlib.pyplot.show()
+    plt.show(block=True)
 
     # to plot this data, take from https://scikit-learn.org/stable/auto_examples/neighbors/plot_regression.html
     # #################################
     # Fit regression model
-    n_neighbors = 5
-
-    X = xi.reshape(-1, 1)
+    x = xi.reshape(-1, 1)
     y = y_i.reshape(-1, 1)
-    T = xi_test.reshape(-1, 1)
+    t = xi_test.reshape(-1, 1)
     # for i, weights in enumerate(['uniform', 'distance']):
     for i, n_neighbors in enumerate([2, 5, 10, 12]):
         knn = neighbors.KNeighborsRegressor(n_neighbors, weights='uniform')
-        y_ = knn.fit(X, y).predict(T)  # changed from T to X
-        matplotlib.pyplot.subplot(4, 1, i + 1)
-        matplotlib.pyplot.scatter(xi_test.reshape(-1, 1), y_i_test.reshape(-1, 1), c='grey', label='test data')
-        matplotlib.pyplot.scatter(X, y, c='k', label='training data')
-        matplotlib.pyplot.plot(T, y_, c='g', label='prediction')  # changed from T to X
-        matplotlib.pyplot.axis('tight')
-        matplotlib.pyplot.legend()
-        matplotlib.pyplot.title("KNeighborsRegressor (k = %i, weights = '%s')" % (n_neighbors,
+        y_ = knn.fit(x, y).predict(t)  # changed from t to x
+        plt.subplot(4, 1, i + 1)
+        plt.scatter(xi_test.reshape(-1, 1), y_i_test.reshape(-1, 1), c='grey', label='test data')
+        plt.scatter(x, y, c='k', label='training data')
+        plt.plot(t, y_, c='g', label='prediction')  # changed from t to x
+        plt.axis('tight')
+        plt.legend()
+        plt.title("KNeighborsRegressor (k = %i, weights = '%s')" % (n_neighbors,
                                                                                   'uniform'))
         # plot f(x) for comparison
-        matplotlib.pyplot.plot(true_fx_x_vals, true_fx_y_vals, 'r--',
+        plt.plot(true_fx_x_vals, true_fx_y_vals, 'r--',
                                label='f')  # Create line plot with yvals against xvals
 
-    matplotlib.pyplot.tight_layout()
-    matplotlib.pyplot.show()
+    plt.tight_layout()
+    plt.show(block=True)
 
 
 # given f(x) = 1.8*x_i + 2
@@ -140,11 +136,12 @@ true_func_y_vals_sin_poly = 1.8 * true_func_x_vals + 2  # placeholder for eventu
 # y variable creation
 # ################################################
 arr = []  # 0=y_i, 1=y_i_test, 2=true_fx_yvals_train, 3=operation 4=x_i_p, 5=x_i_p_test
-arr.append((1.8 * x_i + 2 + e_i, 1.8 * x_i_test + 2 + e_i_test, true_func_y_vals_lin, 'Linear Reg'))
-arr.append((np.tan(1.1 * x_i) + 2 + e_i, np.tan(1.1 * x_i_test) + 2 + e_i_test, true_func_y_vals_tan, 'Tan Func'))
+# arr.append((1.8 * x_i + 2 + e_i, 1.8 * x_i_test + 2 + e_i_test, true_func_y_vals_lin, 'Linear Reg'))
+# arr.append((np.tan(1.1 * x_i) + 2 + e_i, np.tan(1.1 * x_i_test) + 2 + e_i_test, true_func_y_vals_tan, 'Tan Func'))
 arr.append((np.sin(2 * x_i) + 2 + e_i, np.sin(2 * x_i_test) + 2 + e_i_test, true_func_y_vals_sin, 'Sin Func'))
 # (y_i_sin_poly, y_i_test_sin_poly) = (np.sin(2 * x_i) + 2 + e_i,np.sin(2 * x_i_test) + 2 + e_i_test)
 
 # placeholder for loop that will generate 20 different models for 2.8. Models will then be passed into function.
 
-[eval_Knn_Performance(data) for data in arr]
+for data in arr:
+    eval_Knn_Performance(data)
