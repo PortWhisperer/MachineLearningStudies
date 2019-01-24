6d6df1cfc7f2a150c6844fbe32b6123bdf7f4100
PortWhisperer <davidskyniu@gmail.com>
2019-01-23 23:50:56 -0600

    RMSE/Graphing function eval_Knn_Performance() tested working.

--- ML_hw_1.py
+++ ML_hw_1.py
@@ -4,91 +4,58 @@ from sklearn import neighbors
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 
-# given f(x) = 1.8*x_i + 2
-x_i, x_i_test = numpy.sort(numpy.random.uniform(-1, 1, 100), axis=0), \
-                numpy.sort(numpy.random.uniform(-1, 1, 10000), axis=0)
 
-# https://thecuriousastronomer.wordpress.com/2014/06/26/what-does-a-1-sigma-3-sigma-or-5-sigma-detection-mean/
-e_i, e_i_test = numpy.random.normal(0, 1, 100), \
-                numpy.random.normal(0, 1, 10000)
-
-true_func_x_vals = numpy.linspace(-1, 1, 10000)
-
-
-# y variable creation
-# ################################################
-
-y_i_linear, y_i_test_linear = 1.8 * x_i + 2 + e_i, \
-                1.8 * x_i_test + 2 + e_i_test  # given y_i = f(x) + e_i
-
-y_i_tan, y_i_test_tan = numpy.tan(1.1 * x_i) + 2 + e_i, \
-                        numpy.tan(1.1 * x_i_test) + 2 + e_i_test  # given y_i = f(x) + e_i
-
-y_i_sin, y_i_test_sin = numpy.sin(2*x_i) + 2 + e_i, \
-                        numpy.sin(2*x_i_test) + 2 + e_i_test  # given y_i = f(x) + e_i
-
-# true function points
-# ################################################
-true_func_y_vals = 1.8 * true_func_x_vals + 2  # Grid of 0.01 spacing from -1 to 1
-true_func_y_vals_tan = 1.8 * true_func_x_vals + 2  # Grid of 0.01 spacing from -1 to 1
-true_func_y_vals_sin = 1.8 * true_func_x_vals + 2  # Grid of 0.01 spacing from -1 to 1
-true_func_y_vals_sin_poly = 1.8 * true_func_x_vals + 2  # Grid of 0.01 spacing from -1 to 1
-
-
-numpy.random.seed(10)
-
-
-def justgotreal(y_i, y_i_test, true_fx_yvals_train):
-
+def eval_Knn_Performance(y_i, y_i_test, true_fx_yvals_train):
+    xi_test = x_i_test  # pull in x values from global scope
+    xi = x_i
 
     # create points for the true  f(x) function
     true_fx_x_vals = numpy.linspace(-1, 1, 10000)
-    true_fx_y_vals = 1.8 * true_fx_x_vals + 2  # Grid of 0.01 spacing from -1 to 1
+    true_fx_y_vals = true_fx_yvals_train
 
     # matplotlib.pyplot.scatter(x_i_test, y_i_test)
     # matplotlib.pyplot.scatter(x_i, y_i)
     # matplotlib.pyplot.plot(true_fx_xvals, true_fx_yvals, 'r--', label='f')  # Plot true f(x) function
     # matplotlib.pyplot.show()
 
-    true_fx_xvals_trainset = numpy.linspace(-1,1,100)
+    true_fx_xvals_trainset = numpy.linspace(-1, 1, 100)
 
     # plot f(x) against the larger x,y scatterplot with 10k observations
     # may not be needed if we plot against the train set since f(x) there is plotted from (-1,1) already
     # ###################################################
     true_fx_yvals_trainset = 1.8 * true_fx_xvals_trainset + 2
 
-
     # Plot line of best fit for our fitted model
     # https://stackoverflow.com/questions/22239691/code-for-line-of-best-fit-of-a-scatter-plot-in-python
     # #########################################################################
-    matplotlib.pyplot.plot(numpy.unique(x_i), numpy.poly1d(numpy.polyfit(x_i, y_i, 1))(numpy.unique(x_i)),'orange')
+
+    matplotlib.pyplot.plot(numpy.unique(xi), numpy.poly1d(numpy.polyfit(xi, y_i, 1))(numpy.unique(xi)), 'orange')
 
     # Plot k-NN for k=2 through k=15
     # code found at https://www.analyticsvidhya.com/blog/2018/08/k-nearest-neighbor-introduction-regression-python/
 
-    rmse_val = [] # to store rmse values for different k
+    rmse_val = []  # to store rmse values for different k
     rmse_val_train = []
-    for K in range(1,15):
-        K = K+1
 
-        model = neighbors.KNeighborsRegressor(n_neighbors = K) # create the model
-        model.fit(x_i.reshape(-1, 1), y_i)  # fit the KNN model
+    for K in range(1, 15):
+        K = K + 1
 
-        pred  = model.predict(x_i_test.reshape(-1, 1))
-        pred_train = model.predict(x_i.reshape(-1, 1))  # make prediction on test set
+        model = neighbors.KNeighborsRegressor(n_neighbors=K)  # create the model
+        model.fit(xi.reshape(-1, 1), y_i)  # fit the KNN model
 
+        pred = model.predict(xi_test.reshape(-1, 1))
+        pred_train = model.predict(xi.reshape(-1, 1))  # make prediction on test set
 
         error = sqrt(mean_squared_error(y_i_test, pred))  # calculate rmse against test data using y hat, not f(x)
-        error_train = sqrt(mean_squared_error(y_i, pred_train)) # calculate mse against training data
-
+        error_train = sqrt(mean_squared_error(y_i, pred_train))  # calculate mse against training data
 
         rmse_val.append(error)  # store rmse values
         rmse_val_train.append(error_train)
 
     # Run linear regression model
     # ###########################
-    lin_reg_model_test = numpy.polyfit(x_i, y_i, deg=1)
-    lin_reg_model_train = numpy.polyfit(x_i_test, y_i_test, deg=1)
+    lin_reg_model_test = numpy.polyfit(xi, y_i, deg=1)
+    lin_reg_model_train = numpy.polyfit(xi_test, y_i_test, deg=1)
 
     lin_reg_model_pred_train = lin_reg_model_train[0] * numpy.linspace(-1, 1, 100) + lin_reg_model_train[1]
     lin_reg_model_pred_test = lin_reg_model_test[0] * numpy.linspace(-1, 1, 10000) + lin_reg_model_test[1]
@@ -99,14 +66,12 @@ def justgotreal(y_i, y_i_test, true_fx_yvals_train):
     # plot results
     # RMSE vs log(k)
     k = numpy.linspace(2.0, 15.0, 14)  # create for matplotlib plotting purposes
-    fig, (ax1, ax2) = matplotlib.pyplot.subplots(2, 1) # create subplots
+    fig, (ax1, ax2) = matplotlib.pyplot.subplots(2, 1)  # create subplots
     fig.subplots_adjust(hspace=0.5)
 
     ax1.plot(numpy.log(1.0 / k), numpy.array(rmse_val), c='r', label='RMSE')
     ax1.plot(numpy.log(1.0 / k), numpy.array(rmse_val_train), c='black', label='In sample RMSE')
-    ax1.axhline(y=error_lin_reg_test, c='g', dashes=(5,1), label="Lin Reg RMSE")
-
-
+    ax1.axhline(y=error_lin_reg_test, c='g', dashes=(5, 1), label="Lin Reg RMSE")
     ax1.set_xlabel('Log(1/k)')
     ax1.set_ylabel('RMSE')
     ax1.legend(loc='upper left')
@@ -114,8 +79,7 @@ def justgotreal(y_i, y_i_test, true_fx_yvals_train):
     # RMSE vs K
     ax2.plot(k, numpy.array(rmse_val), c='r', label='RMSE')
     ax2.plot(k, numpy.array(rmse_val_train), c='black', label='In sample RMSE')
-    ax2.axhline(y=error_lin_reg_test, c='g', dashes=(5,1), label="Lin Reg RMSE")
-
+    ax2.axhline(y=error_lin_reg_test, c='g', dashes=(5, 1), label="Lin Reg RMSE")
     ax2.set_xlabel('K')
     ax2.set_ylabel('RMSE')
     ax2.legend(loc='upper left')
@@ -126,24 +90,56 @@ def justgotreal(y_i, y_i_test, true_fx_yvals_train):
     # Fit regression model
     n_neighbors = 5
 
-    X = x_i.reshape(-1, 1)
+    X = xi.reshape(-1, 1)
     y = y_i.reshape(-1, 1)
-    T = x_i_test.reshape(-1, 1)
+    T = xi_test.reshape(-1, 1)
     # for i, weights in enumerate(['uniform', 'distance']):
     for i, n_neighbors in enumerate([2, 5, 10, 12]):
         knn = neighbors.KNeighborsRegressor(n_neighbors, weights='uniform')
         y_ = knn.fit(X, y).predict(T)  # changed from T to X
         matplotlib.pyplot.subplot(4, 1, i + 1)
-        matplotlib.pyplot.scatter(x_i_test.reshape(-1, 1),y_i_test.reshape(-1,1), c='grey', label='test data')
+        matplotlib.pyplot.scatter(xi_test.reshape(-1, 1), y_i_test.reshape(-1, 1), c='grey', label='test data')
         matplotlib.pyplot.scatter(X, y, c='k', label='training data')
-        matplotlib.pyplot.plot(T,y_, c='g', label='prediction')  # changed from T to X
+        matplotlib.pyplot.plot(T, y_, c='g', label='prediction')  # changed from T to X
         matplotlib.pyplot.axis('tight')
         matplotlib.pyplot.legend()
         matplotlib.pyplot.title("KNeighborsRegressor (k = %i, weights = '%s')" % (n_neighbors,
                                                                                   'uniform'))
-    # plot f(x) for comparison
-        matplotlib.pyplot.plot(true_fx_x_vals, true_fx_y_vals, 'r--', label='f')  # Create line plot with yvals against xvals
+        # plot f(x) for comparison
+        matplotlib.pyplot.plot(true_fx_x_vals, true_fx_y_vals, 'r--',
+                               label='f')  # Create line plot with yvals against xvals
 
     matplotlib.pyplot.tight_layout()
     matplotlib.pyplot.show()
 
+
+# given f(x) = 1.8*x_i + 2
+x_i, x_i_test = numpy.sort(numpy.random.uniform(-1, 1, 100), axis=0), \
+                numpy.sort(numpy.random.uniform(-1, 1, 10000), axis=0)
+
+# https://thecuriousastronomer.wordpress.com/2014/06/26/what-does-a-1-sigma-3-sigma-or-5-sigma-detection-mean/
+e_i, e_i_test = numpy.random.normal(0, 1, 100), \
+                numpy.random.normal(0, 1, 10000)
+
+true_func_x_vals = numpy.linspace(-1, 1, 10000)
+
+# y variable creation
+# ################################################
+y_i_linear, y_i_test_linear = 1.8 * x_i + 2 + e_i, \
+                              1.8 * x_i_test + 2 + e_i_test  # given y_i = f(x) + e_i
+
+y_i_tan, y_i_test_tan = numpy.tan(1.1 * x_i) + 2 + e_i, \
+                        numpy.tan(1.1 * x_i_test) + 2 + e_i_test  # given y_i = f(x) + e_i
+
+y_i_sin, y_i_test_sin = numpy.sin(2 * x_i) + 2 + e_i, \
+                        numpy.sin(2 * x_i_test) + 2 + e_i_test  # given y_i = f(x) + e_i
+
+# placeholder for loop that will generate 20 different models for 2.8. Models will then be passed into function.
+
+
+# true function points
+# ################################################
+true_func_y_vals = 1.8 * true_func_x_vals + 2  # Grid of 0.01 spacing from -1 to 1
+true_func_y_vals_tan = 1.8 * true_func_x_vals + 2  # Grid of 0.01 spacing from -1 to 1
+true_func_y_vals_sin = 1.8 * true_func_x_vals + 2  # Grid of 0.01 spacing from -1 to 1
+true_func_y_vals_sin_poly = 1.8 * true_func_x_vals + 2  # Grid of 0.01 spacing from -1 to 1
