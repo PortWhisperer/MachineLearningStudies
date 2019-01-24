190377037a6f259053d7f891fb7ec06cdb7b3252
PortWhisperer <davidskyniu@gmail.com>
2019-01-24 03:04:28 -0600

    convert func to accept *args

--- ML_hw_1.py
+++ ML_hw_1.py
@@ -1,16 +1,23 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot
 from sklearn import neighbors
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 
 
-def eval_Knn_Performance(y_i, y_i_test, true_fx_yvals_train):
-    xi_test = x_i_test  # pull in x values from global scope
-    xi = x_i
+def eval_Knn_Performance(var_list):
+    if len(var_list[0]) == 5:
+        y_i, y_i_test, true_fx_yvals_train, x_i_p, x_i_p_test = var_list[0]
+    else:
+        y_i, y_i_test, true_fx_yvals_train = var_list[0]
+
+    if x_i_p and x_i_p_test:
+        xi_test, xi = var_list[3], var_list[4]  # pull in x values from list if avail
+    else:
+        xi_test, xi = x_i_test, x_i  # else pull in x values from global scope and avoid shadowing
 
     # create points for the true  f(x) function
-    true_fx_x_vals = numpy.linspace(-1, 1, 10000)
+    true_fx_x_vals = np.linspace(-1, 1, 10000)
     true_fx_y_vals = true_fx_yvals_train
 
     # matplotlib.pyplot.scatter(x_i_test, y_i_test)
@@ -18,7 +25,7 @@ def eval_Knn_Performance(y_i, y_i_test, true_fx_yvals_train):
     # matplotlib.pyplot.plot(true_fx_xvals, true_fx_yvals, 'r--', label='f')  # Plot true f(x) function
     # matplotlib.pyplot.show()
 
-    true_fx_xvals_trainset = numpy.linspace(-1, 1, 100)
+    true_fx_xvals_trainset = np.linspace(-1, 1, 100)
 
     # plot f(x) against the larger x,y scatterplot with 10k observations
     # may not be needed if we plot against the train set since f(x) there is plotted from (-1,1) already
@@ -29,7 +36,7 @@ def eval_Knn_Performance(y_i, y_i_test, true_fx_yvals_train):
     # https://stackoverflow.com/questions/22239691/code-for-line-of-best-fit-of-a-scatter-plot-in-python
     # #########################################################################
 
-    matplotlib.pyplot.plot(numpy.unique(xi), numpy.poly1d(numpy.polyfit(xi, y_i, 1))(numpy.unique(xi)), 'orange')
+    matplotlib.pyplot.plot(np.unique(xi), np.poly1d(np.polyfit(xi, y_i, 1))(np.unique(xi)), 'orange')
 
     # Plot k-NN for k=2 through k=15
     # code found at https://www.analyticsvidhya.com/blog/2018/08/k-nearest-neighbor-introduction-regression-python/
@@ -54,31 +61,31 @@ def eval_Knn_Performance(y_i, y_i_test, true_fx_yvals_train):
 
     # Run linear regression model
     # ###########################
-    lin_reg_model_test = numpy.polyfit(xi, y_i, deg=1)
-    lin_reg_model_train = numpy.polyfit(xi_test, y_i_test, deg=1)
+    lin_reg_model_test = np.polyfit(xi, y_i, deg=1)
+    lin_reg_model_train = np.polyfit(xi_test, y_i_test, deg=1)
 
-    lin_reg_model_pred_train = lin_reg_model_train[0] * numpy.linspace(-1, 1, 100) + lin_reg_model_train[1]
-    lin_reg_model_pred_test = lin_reg_model_test[0] * numpy.linspace(-1, 1, 10000) + lin_reg_model_test[1]
+    lin_reg_model_pred_train = lin_reg_model_train[0] * np.linspace(-1, 1, 100) + lin_reg_model_train[1]
+    lin_reg_model_pred_test = lin_reg_model_test[0] * np.linspace(-1, 1, 10000) + lin_reg_model_test[1]
 
     error_lin_reg_test = sqrt(mean_squared_error(y_i_test, lin_reg_model_pred_test))  # out of sample RMSE = 1.0233
     error_lin_reg_train = sqrt(mean_squared_error(y_i, lin_reg_model_pred_train))  # in sample RMSE = .93796
 
     # plot results
     # RMSE vs log(k)
-    k = numpy.linspace(2.0, 15.0, 14)  # create for matplotlib plotting purposes
+    k = np.linspace(2.0, 15.0, 14)  # create for matplotlib plotting purposes
     fig, (ax1, ax2) = matplotlib.pyplot.subplots(2, 1)  # create subplots
     fig.subplots_adjust(hspace=0.5)
 
-    ax1.plot(numpy.log(1.0 / k), numpy.array(rmse_val), c='r', label='RMSE')
-    ax1.plot(numpy.log(1.0 / k), numpy.array(rmse_val_train), c='black', label='In sample RMSE')
+    ax1.plot(np.log(1.0 / k), np.array(rmse_val), c='r', label='RMSE')
+    ax1.plot(np.log(1.0 / k), np.array(rmse_val_train), c='black', label='In sample RMSE')
     ax1.axhline(y=error_lin_reg_test, c='g', dashes=(5, 1), label="Lin Reg RMSE")
     ax1.set_xlabel('Log(1/k)')
     ax1.set_ylabel('RMSE')
     ax1.legend(loc='upper left')
 
     # RMSE vs K
-    ax2.plot(k, numpy.array(rmse_val), c='r', label='RMSE')
-    ax2.plot(k, numpy.array(rmse_val_train), c='black', label='In sample RMSE')
+    ax2.plot(k, np.array(rmse_val), c='r', label='RMSE')
+    ax2.plot(k, np.array(rmse_val_train), c='black', label='In sample RMSE')
     ax2.axhline(y=error_lin_reg_test, c='g', dashes=(5, 1), label="Lin Reg RMSE")
     ax2.set_xlabel('K')
     ax2.set_ylabel('RMSE')
@@ -114,43 +121,30 @@ def eval_Knn_Performance(y_i, y_i_test, true_fx_yvals_train):
 
 
 # given f(x) = 1.8*x_i + 2
-x_i, x_i_test = numpy.sort(numpy.random.uniform(-1, 1, 100), axis=0), \
-                numpy.sort(numpy.random.uniform(-1, 1, 10000), axis=0)
+x_i, x_i_test = np.sort(np.random.uniform(-1, 1, 100), axis=0), \
+                np.sort(np.random.uniform(-1, 1, 10000), axis=0)
 
 # https://thecuriousastronomer.wordpress.com/2014/06/26/what-does-a-1-sigma-3-sigma-or-5-sigma-detection-mean/
-e_i, e_i_test = numpy.random.normal(0, 1, 100), \
-                numpy.random.normal(0, 1, 10000)
+e_i, e_i_test = np.random.normal(0, 1, 100), \
+                np.random.normal(0, 1, 10000)
 
-true_func_x_vals = numpy.linspace(-1, 1, 10000)
+true_func_x_vals = np.linspace(-1, 1, 10000)
 
 # Plot actual function
 # ################################################
-true_func_y_vals = 1.8 * true_func_x_vals + 2
-true_func_y_vals_tan = numpy.tan(1.1 * true_func_x_vals) + 2
-true_func_y_vals_sin = numpy.sin(2 * true_func_x_vals) + 2
+true_func_y_vals_lin = 1.8 * true_func_x_vals + 2
+true_func_y_vals_tan = np.tan(1.1 * true_func_x_vals) + 2
+true_func_y_vals_sin = np.sin(2 * true_func_x_vals) + 2
 true_func_y_vals_sin_poly = 1.8 * true_func_x_vals + 2  # placeholder for eventual polynomial func
-
 
 # y variable creation
 # ################################################
-y_i_linear, y_i_test_linear = 1.8 * x_i + 2 + e_i, \
-                              1.8 * x_i_test + 2 + e_i_test  # given y_i = f(x) + e_i
-
-y_i_tan, y_i_test_tan = numpy.tan(1.1 * x_i) + 2 + e_i, \
-                        numpy.tan(1.1 * x_i_test) + 2 + e_i_test  # given y_i = f(x) + e_i
-
-y_i_sin, y_i_test_sin = numpy.sin(2 * x_i) + 2 + e_i, \
-                        numpy.sin(2 * x_i_test) + 2 + e_i_test  # given y_i = f(x) + e_i
+arr = []  # 0=y_i, 1=y_i_test, 2=true_fx_yvals_train, 3=operation 4=x_i_p, 5=x_i_p_test
+arr.append((1.8 * x_i + 2 + e_i, 1.8 * x_i_test + 2 + e_i_test, true_func_y_vals_lin, 'Linear Reg'))
+arr.append((np.tan(1.1 * x_i) + 2 + e_i, np.tan(1.1 * x_i_test) + 2 + e_i_test, true_func_y_vals_tan, 'Tan Func'))
+arr.append((np.sin(2 * x_i) + 2 + e_i, np.sin(2 * x_i_test) + 2 + e_i_test, true_func_y_vals_sin, 'Sin Func'))
+# (y_i_sin_poly, y_i_test_sin_poly) = (np.sin(2 * x_i) + 2 + e_i,np.sin(2 * x_i_test) + 2 + e_i_test)
 
 # placeholder for loop that will generate 20 different models for 2.8. Models will then be passed into function.
 
-
-
-# 2.1-2.5
-eval_Knn_Performance(y_i=y_i_linear, y_i_test=y_i_test_linear, true_fx_yvals_train=true_func_y_vals)
-# 2.6
-eval_Knn_Performance(y_i=y_i_tan, y_i_test=y_i_test_linear, true_fx_yvals_train=true_func_y_vals)
-# 2.7
-eval_Knn_Performance(y_i=y_i_sin, y_i_test=y_i_test_linear, true_fx_yvals_train=true_func_y_vals)
-# 2.8
-eval_Knn_Performance(y_i=y_i_linear, y_i_test=y_i_test_linear, true_fx_yvals_train=true_func_y_vals)
+[eval_Knn_Performance(data) for data in arr]
